2f4a67d5a196f9795bdaae1b6e5618dd6e089537
Ahmed Hossam <63050133+7oSkaaa@users.noreply.github.com>
2022-01-21 00:37:12 +0200

    Update Bubble_Sort.cpp

--- Algorithms_Code/C++/Bubble_Sort.cpp
+++ Algorithms_Code/C++/Bubble_Sort.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 void Bubble_Sort(vector < int >& nums){
     int n = nums.size();
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n - 1; i++){
         bool is_sorted = true;
-        for(int j = i; j < n; j++){
-            if(nums[j] < nums[i])
-                swap(nums[i], nums[j]), is_sorted = false;
+        for(int j = 0; j < n - i - 1; j++){
+            if(nums[j] < nums[j + 1])
+                swap(nums[j], nums[j + 1]), is_sorted = false;
         }
         if(is_sorted) return;
     }
