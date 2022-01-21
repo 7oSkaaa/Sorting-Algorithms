614dfc2729dda13ba70155977442a473bf5c05d6
Ahmed Hossam <63050133+7oSkaaa@users.noreply.github.com>
2022-01-21 19:40:46 +0200

    Update Insertion_Sort.cpp

--- Algorithms_Code/C++/Insertion_Sort.cpp
+++ Algorithms_Code/C++/Insertion_Sort.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void Insertion_Sort(vector < int >& nums){
     int n = nums.size();
-    for(int i = 0; i < n; i++){
+    for(int i = 1; i < n; i++){
         int value = nums[i], j = i - 1;
         while(j >= 0 && nums[j] > value)
             nums[j + 1] = nums[j], j--;
