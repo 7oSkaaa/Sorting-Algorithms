cbd872941905661d20fed26bb473ed123baa7bd0
Ahmed Hossam <63050133+7oSkaaa@users.noreply.github.com>
2022-01-28 00:06:52 +0200

    Update Selection_Sort.cpp

--- Algorithms_Code/C++/Selection_Sort.cpp
+++ Algorithms_Code/C++/Selection_Sort.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void Selection_Sort(vector < int >& nums){
     int n = nums.size();
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n - 1; i++){
         int min = i;
         for(int j = i + 1; j < n; j++){
             if(nums[j] < nums[min])
